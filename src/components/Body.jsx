1b388c35eadf15f494f83e787fef1f00e34921e7
RItu Raj <rajritu2084@gmail.com>
2024-06-25 22:37:19 +0530

    Lecture 5 Hooks

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 import { resList } from '../utils/mockData';
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // console.log('Clicked');
+            const filteredRestaurant = listOfRestaurant.filter(
+              (restaurant) => restaurant.info.avgRating > 4.5
+            );
+            // console.log(filteredRestaurant);
+            setListOfRestaurant(filteredRestaurant);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="restaurant-container">
-        {resList.map((restaurant) => {
+        {listOfRestaurant.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
