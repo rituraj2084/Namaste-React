c90d7f87f496e3a40ba4efdc668e50b28bf31864
RItu Raj <rajritu2084@gmail.com>
2024-07-01 18:49:58 +0530

    Lecture 9 Custom Hooks

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -10,22 +10,15 @@ class UserClass extends React.Component {
     };
   }
   async componentDidMount() {
-    // Fetch data from the GitHub API using the specified URL.
     const data = await fetch('https://api.github.com/users/rituraj2084');
-
-    // Parse the response as JSON and store it in the 'json' variable.
     const json = await data.json();
 
-    // Log the fetched data to the console.
-    console.log(json);
     this.setState({
       userInfo: json,
     });
   }
   render() {
-    // const { name, location } = this.props;
     const { name, location, avatar_url } = this.state.userInfo;
-    // console.log(this.state.userInfo.name);
     return (
       <div>
         <img src={avatar_url} alt="profle pic" />
