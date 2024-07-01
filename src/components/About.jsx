c5b4a90b18418a7c6bd2e7141067e6fe26d28ac9
RItu Raj <rajritu2084@gmail.com>
2024-07-01 18:37:38 +0530

    Lecture 8 Class Based Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,16 @@ import UserClass from './UserClass';
 class About extends React.Component {
   constructor(props) {
     super(props);
-    console.log('Parent Constructor');
   }
   componentDidMount() {
-    console.log('Parent component did mount');
+    // console.log("Parents Component Did Mount Called");
   }
   render() {
-    console.log('Parent render');
     return (
       <div>
         <h1>About us Page</h1>
         <h2>This is about us page</h2>
         <UserClass name="First" location="Gurugram" />
-        <UserClass name="Second" location="Gurugram" />
-        <UserClass name="Third" location="Gurugram" />
       </div>
     );
   }
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,30 +3,32 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      count2: 2,
+      userInfo: {
+        name: '',
+        location: '',
+      },
     };
-    console.log(this.props.name + ' Child constructor');
   }
-  componentDidMount() {
-    console.log(this.props.name + ' Child component did mount');
+  async componentDidMount() {
+    // Fetch data from the GitHub API using the specified URL.
+    const data = await fetch('https://api.github.com/users/rituraj2084');
+
+    // Parse the response as JSON and store it in the 'json' variable.
+    const json = await data.json();
+
+    // Log the fetched data to the console.
+    console.log(json);
+    this.setState({
+      userInfo: json,
+    });
   }
   render() {
-    const { name, location } = this.props;
-    const { count } = this.state;
-    console.log(this.props.name + ' Child render');
+    // const { name, location } = this.props;
+    const { name, location, avatar_url } = this.state.userInfo;
+    // console.log(this.state.userInfo.name);
     return (
       <div>
-        <h3>Count: {count}</h3>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Count Increse
-        </button>
+        <img src={avatar_url} alt="profle pic" />
         <h1>Name: {name}</h1>
         <h2>Contact: @rituraj2084</h2>
         <h2>Location: {location}</h2>
